150b91f8433e9853a3ee1df16bfa55efd427aa51
keenan warouw <keenanwarouw@10-60-49-142.0200.bsd.id.iosda.org>
2018-07-04 14:23:30 +0700

    Initial Commit

---
+++ CollaborationTests/CollaborationTests.swift
@@ -0,0 +1,36 @@
+//
+//  CollaborationTests.swift
+//  CollaborationTests
+//
+//  Created by keenan warouw on 04/07/18.
+//  Copyright © 2018 keenan warouw. All rights reserved.
+//
+
+import XCTest
+@testable import Collaboration
+
+class CollaborationTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
